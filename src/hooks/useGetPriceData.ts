b075d13fabc97f612bbb32a91f7785f9833a9b80
venusgalstar <venusgalstar@gmail.com>
2022-09-15 17:30:10 +0000

    init swap frontend

---
+++ src/hooks/useGetPriceData.ts
@@ -0,0 +1,87 @@
+import { BigNumber } from 'ethers';
+import BN from "bignumber.js";
+import { useEffect, useState } from 'react'
+import { useSingleCallResult } from 'state/multicall/hooks';
+import dummyPriceData from '../data/tokens.json';
+import { useTokenContract } from './useContract';
+import { CAKE } from '../constants';
+
+type ApiResponse = {
+  updated_at: string
+  data: {
+    [key: string]: {
+      name: string
+      symbol: string
+      price: string
+      price_BNB: string
+    }
+  }
+}
+
+// const api = 'https://api.pancakeswap.info/api/tokens'
+
+const useGetPriceData = () => {
+  const [data, setData] = useState<ApiResponse | null>(null)
+  const busdaddr = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+  const wbnbAddr = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+  const brisBnbLpAddr = "0x8B3b22B12bF85F8bC1C6e17Bd09329FbAaFF5f7D";
+  const bnbBusdLpAddr = "0x5E7239dFc60D54a4c469bfb3a1804C27a4A214D5";
+  const BIG_TEN = new BN(10);
+  
+  const busdContract = useTokenContract(busdaddr, false);
+  const busdAmount = new BN(useSingleCallResult(busdContract, "balanceOf", [bnbBusdLpAddr])?.result?.[0]?.toString());
+  const wbnbContract = useTokenContract(wbnbAddr, false);
+  let wbnbAmount = new BN(useSingleCallResult(wbnbContract, "balanceOf", [bnbBusdLpAddr])?.result?.[0]?.toString());
+  const bnbPriceInBusd = busdAmount?.div(wbnbAmount);
+
+  const brisContract = useTokenContract(CAKE.address, false);
+  const brisAmount = new BN(useSingleCallResult(brisContract, "balanceOf", [brisBnbLpAddr])?.result?.[0]?.toString());
+  wbnbAmount = new BN(useSingleCallResult(wbnbContract, "balanceOf", [brisBnbLpAddr])?.result?.[0]?.toString());
+  const brisPriceInBnb = wbnbAmount?.div(brisAmount);
+  
+  const brisPriceInBusd = bnbPriceInBusd.times(brisPriceInBnb);
+  console.log("[DAVID] ", bnbPriceInBusd?.toString(), brisPriceInBnb?.toString(), brisPriceInBusd.toString());
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const response = await fetch(api)
+        // const res: ApiResponse = await response.json();
+
+        const res = JSON.parse(JSON.stringify(dummyPriceData));
+        setData(res)
+      } catch (error) {
+        console.error('Unable to fetch price data:', error)
+      }
+    }
+
+    fetchData()
+  }, [setData])
+
+  return data
+}
+
+export const useGetPriceFromFarm = () => {
+  const busdaddr = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+  const wbnbAddr = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+  const brisBnbLpAddr = "0x8B3b22B12bF85F8bC1C6e17Bd09329FbAaFF5f7D";
+  const bnbBusdLpAddr = "0x5E7239dFc60D54a4c469bfb3a1804C27a4A214D5";
+  
+  const busdContract = useTokenContract(busdaddr, false);
+  const busdAmount = new BN(useSingleCallResult(busdContract, "balanceOf", [bnbBusdLpAddr])?.result?.[0]?.toString());
+  const wbnbContract = useTokenContract(wbnbAddr, false);
+  let wbnbAmount = new BN(useSingleCallResult(wbnbContract, "balanceOf", [bnbBusdLpAddr])?.result?.[0]?.toString());
+  const bnbPriceInBusd = busdAmount?.div(wbnbAmount);
+
+  const brisContract = useTokenContract(CAKE.address, false);
+  const brisAmount = new BN(useSingleCallResult(brisContract, "balanceOf", [brisBnbLpAddr])?.result?.[0]?.toString());
+  wbnbAmount = new BN(useSingleCallResult(wbnbContract, "balanceOf", [brisBnbLpAddr])?.result?.[0]?.toString());
+  const brisPriceInBnb = wbnbAmount?.div(brisAmount);
+  
+  const brisPriceInBusd = bnbPriceInBusd.times(brisPriceInBnb);
+  console.log("[DAVID] ", bnbPriceInBusd?.toString(), brisPriceInBnb?.toString(), brisPriceInBusd.toString());
+  
+  return parseFloat(brisPriceInBusd?.toString());
+}
+
+export default useGetPriceData
---
+++ vital-folders/sdk/dist/constants.d.ts
@@ -0,0 +1,35 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    BSCTESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x6d32088ae9283487A1eC84C02d02466DcB684D04";
+export declare const INIT_CODE_HASH = "0x8abdde17d7f39d9f7c1fe4ea452a368b12df9a1ea18a9be73833c8017e980c23";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
