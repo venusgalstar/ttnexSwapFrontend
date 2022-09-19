4cf167250f6a36b96e1e1bb3acf6416a467951a0
venusgalstar <venusgalstar@gmail.com>
2022-09-19 13:41:06 +0000

    remove menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,15 +31,15 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: 'https://ttndex.com/pools',
   },
-  {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: 'https://ttndex.com/lottery',
-    // status: {
-    //   text: 'WIN',
-    //   color: 'warning',
-    // },
-  },
+  // {
+  //   // label: 'Lottery',
+  //   // icon: 'TicketIcon',
+  //   // href: 'https://ttndex.com/lottery',
+  //   // status: {
+  //   //   text: 'WIN',
+  //   //   color: 'warning',
+  //   // },
+  // },
   {
     label: 'Referral Program',
     icon: 'ReferralIcon',
@@ -100,40 +100,40 @@ const config: MenuEntry[] = [
   //   icon: 'IfoIcon',
   //   href: 'https://pancakeswap.finance/ifo',
   // },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Team',
-        href: 'https://docs.ttndex.com/bridgeswap/our-team',
-      },
-      // {
-      //   label: 'Contact',
-      //   href: 'https://t.me/officialflutterswap',
-      // },
-      // {
-      //   label: 'Voting',
-      //   href: 'https://voting.pancakeswap.finance',
-      // },
-      // {
-      //   label: 'Github',
-      //   href: 'https://github.com/BridgeSwap-Exchange',
-      // },
-      // {
-      //   label: 'Docs',
-      //   href: 'https://docs.ttndex.com',
-      // },
-      // {
-      //   label: 'Blog',
-      //   href: 'https://bridgeswap.medium.com/',
-      // },
-      // {
-      //   label: 'Merch',
-      //   href: 'https://pancakeswap.creator-spring.com/',
-      // },
-    ],
-  },
+  // {
+  //   label: 'More',
+  //   icon: 'MoreIcon',
+  //   items: [
+  //     {
+  //       label: 'Team',
+  //       href: 'https://docs.ttndex.com/bridgeswap/our-team',
+  //     },
+  //     // {
+  //     //   label: 'Contact',
+  //     //   href: 'https://t.me/officialflutterswap',
+  //     // },
+  //     // {
+  //     //   label: 'Voting',
+  //     //   href: 'https://voting.pancakeswap.finance',
+  //     // },
+  //     // {
+  //     //   label: 'Github',
+  //     //   href: 'https://github.com/BridgeSwap-Exchange',
+  //     // },
+  //     // {
+  //     //   label: 'Docs',
+  //     //   href: 'https://docs.ttndex.com',
+  //     // },
+  //     // {
+  //     //   label: 'Blog',
+  //     //   href: 'https://bridgeswap.medium.com/',
+  //     // },
+  //     // {
+  //     //   label: 'Merch',
+  //     //   href: 'https://pancakeswap.creator-spring.com/',
+  //     // },
+  //   ],
+  // },
 ]
 
 export default config
