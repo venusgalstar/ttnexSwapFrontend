1d2e2840f44b7e756751461b6a9046134e7b9066
CryptoSnowPrince <topdirector2017@gmail.com>
2022-09-28 19:41:53 +0000

    fix cake price and chainID issue on development mode

--- src/hooks/useGetDocumentTitlePrice.ts
+++ src/hooks/useGetDocumentTitlePrice.ts
@@ -1,22 +1,32 @@
 import { useEffect } from 'react'
-import useGetPriceData from './useGetPriceData'
+// import useGetPriceData from './useGetPriceData'
+import { useGetPriceFromFarm } from './useGetPriceData'
 import { CAKE } from '../constants'
 
 const useGetDocumentTitlePrice = () => {
-  const priceData = useGetPriceData()
+  // const priceData = useGetPriceData()
 
-  const cakePriceUsd = priceData ? parseFloat(priceData.data[CAKE.address].price) : 0
+  // const cakePriceUsd = priceData ? parseFloat(priceData.data[CAKE.address].price) : 0
 
-  const cakePriceUsdString =
-    Number.isNaN(cakePriceUsd) || cakePriceUsd === 0
-      ? ''
-      : ` - $${cakePriceUsd.toLocaleString(undefined, {
-          minimumFractionDigits: 3,
-          maximumFractionDigits: 3,
-        })}`
+  // const cakePriceUsdString =
+  //   Number.isNaN(cakePriceUsd) || cakePriceUsd === 0
+  //     ? ''
+  //     : ` - $${cakePriceUsd.toLocaleString(undefined, {
+  //       minimumFractionDigits: 3,
+  //       maximumFractionDigits: 3,
+  //     })}`
+
+  // useEffect(() => {
+  //   document.title = `TTNDEX${cakePriceUsdString}`
+  // }, [cakePriceUsdString])
+
+  const cakePriceBusd = useGetPriceFromFarm()
 
   useEffect(() => {
-    document.title = `TTNDEX${cakePriceUsdString}`
-  }, [cakePriceUsdString])
+    document.title = `TTNDEX - $${cakePriceBusd.toLocaleString('en-US', {
+      minimumFractionDigits: 3,
+      maximumFractionDigits: 3,
+    })}`
+  }, [cakePriceBusd])
 }
 export default useGetDocumentTitlePrice
